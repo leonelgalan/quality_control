6a89b363c1fe49609ed848030d8fb523b8ecbbbe
Leonel Galan <leonelgalan@gmail.com>
2015-02-06 17:04:57 -0500

    Fixes rubocop latest warnings.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,4 @@ ActiveRecord::Migration.maintain_test_schema!
 
 ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV['AR_LOGGER']
 
-RSpec.configure do |config|
-  config.infer_spec_type_from_file_location!
-end
+RSpec.configure(&:infer_spec_type_from_file_location!)
